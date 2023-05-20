e7d09498340dc931f08e4795dc1c47cbf49f9a44
Marius Iacob <47167361+mariusiacob1@users.noreply.github.com>
2023-05-19 23:46:49 -0700

    pushing everything

--- Snake-AI-PyTorch/game.py
+++ Snake-AI-PyTorch/game.py
@@ -22,9 +22,8 @@ RED = (200,0,0)
 PURPLE = (118, 5, 240)
 GREEN1 = (2, 250, 31)
 GREEN2 = (59, 255, 81)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+BLUE = (11, 2, 245)
+BROWN = (105, 68, 36)
 
 BLOCK_SIZE = 20
 SPEED = 40
@@ -130,14 +129,14 @@ class SnakeGameAI:
 
 
     def _update_ui(self):
-        self.display.fill(BLACK)
+        self.display.fill(BROWN)
 
         for pt in self.snake:
             pygame.draw.rect(self.display, GREEN1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(self.display, GREEN2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
 
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
-        pygame.draw.rect(self.display, PURPLE, pygame.Rect(self.poison.x, self.poison.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, BLUE, pygame.Rect(self.poison.x, self.poison.y, BLOCK_SIZE, BLOCK_SIZE))
 
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
