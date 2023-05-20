51c6bd3d22368ba848d48c8d425e9f413c55f1c3
Marius Iacob <47167361+mariusiacob1@users.noreply.github.com>
2023-05-19 19:12:42 -0700

    Added poison food to game.py

--- Snake-AI-PyTorch/game.py
+++ Snake-AI-PyTorch/game.py
@@ -14,11 +14,12 @@ class Direction(Enum):
     UP = 3
     DOWN = 4
 
-Point = namedtuple('Point', 'x, y')
+Point = namedtuple('Point', 'x, y')  # x = width and y = height
 
 # rgb colors
 WHITE = (255, 255, 255)
 RED = (200,0,0)
+PURPLE = (118, 5, 240)
 GREEN1 = (2, 250, 31)
 GREEN2 = (59, 255, 81)
 BLUE1 = (0, 0, 255)
@@ -30,11 +31,11 @@ SPEED = 40
 
 class SnakeGameAI:
 
-    def __init__(self, w=640, h=480):
-        self.w = w
-        self.h = h
+    def __init__(self, width=640, height=480):
+        self.width = width
+        self.height = height
         # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
+        self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
         self.reset()
@@ -44,23 +45,33 @@ class SnakeGameAI:
         # init game state
         self.direction = Direction.RIGHT
 
-        self.head = Point(self.w/2, self.h/2)
+        self.head = Point(self.width/2, self.height/2)
         self.snake = [self.head,
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
 
         self.score = 0
         self.food = None
+        self.poison = None
         self._place_food()
+        self._place_poison()
         self.frame_iteration = 0
 
 
     def _place_food(self):
-        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        x = random.randint(0, (self.width-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        y = random.randint(0, (self.height-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
         self.food = Point(x, y)
         if self.food in self.snake:
             self._place_food()
+
+
+    def _place_poison(self):
+        x = random.randint(0, (self.width-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        y = random.randint(0, (self.height-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.poison = Point(x, y)
+        if self.poison in self.snake:
+            self._place_poison()
 
 
     def play_step(self, action):
@@ -90,11 +101,18 @@ class SnakeGameAI:
             self._place_food()
         else:
             self.snake.pop()
+
+        # 5. place new poison or just move
+        if self.head == self.poison:
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
         
-        # 5. update ui and clock
+        # 6. update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
-        # 6. return game over and score
+        
+        # 7. return game over and score
         return reward, game_over, self.score
 
 
@@ -102,7 +120,7 @@ class SnakeGameAI:
         if pt is None:
             pt = self.head
         # hits boundary
-        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+        if pt.x > self.width - BLOCK_SIZE or pt.x < 0 or pt.y > self.height - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
         if pt in self.snake[1:]:
@@ -119,6 +137,7 @@ class SnakeGameAI:
             pygame.draw.rect(self.display, GREEN2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
 
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, PURPLE, pygame.Rect(self.poison.x, self.poison.y, BLOCK_SIZE, BLOCK_SIZE))
 
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
